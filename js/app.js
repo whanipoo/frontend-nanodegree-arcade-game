409251e0443ee9678ed446b25bbfbede2493b8a4
Eva Sims <whanipoo@gmail.com>
2018-09-30 19:02:44 -0500

    Character can't move off the canvas & added the enemies

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // Enemies our player must avoid
 // Variables applied to each of our instances go here
-var Enemy = function(sprite, x, y) {
+var Enemy = function(x, y) {
   // The image for our enemies
   this.sprite = "images/enemy-bug.png";
   this.x = x;
@@ -13,6 +13,7 @@ var Enemy = function(sprite, x, y) {
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
   //Multiply any movement by the dt parameter
+  this.x += 150*dt;
 };
 
 // Draw the enemy on the screen
@@ -45,21 +46,32 @@ Player.prototype.handleInput = function(direction) {
   if (direction === "left") {
     this.x -= 100;
   } else if (direction === "up") {
-    this.y -= 80;
+    this.y -= 83;
   } else if (direction === "right") {
     this.x += 100;
   } else if (direction === "down") {
-    this.y += 80;
+    this.y += 83;
   }
   console.log(`x=${this.x}, y=${this.y}`);
+
+  //Preventing the player from going off the canvas
+  if (this.x < 0) {
+    this.x = 0;
+  } else if (this.x > 400) {
+    this.x = 400;
+  } else if (this.y < -20) {
+    this.y = -20;
+  } else if (this.y > 380) {
+    this.y = 380;
+  }
 };
 
 //Create a player
 let player = new Player("", 200, 300);
 
 let enemyPosition = [55, 140, 230];
-let allEnemies = enemyPosition.map((x, index) => {
-  return new Enemy(-100 * (index + 1), x);
+let allEnemies = enemyPosition.map((y, index) => {
+  return new Enemy((-100 * (index + 1)), y);
 });
 
 // This listens for key presses and sends the keys to your
@@ -74,6 +86,8 @@ document.addEventListener("keyup", function(e) {
 
   player.handleInput(allowedKeys[e.keyCode]);
 });
+
+console.log(allEnemies);
 
 //Udacity's comments
 
